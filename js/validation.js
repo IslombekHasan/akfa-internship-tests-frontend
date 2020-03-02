80bab6e17d2d98e02bace966c654ad2e56626093
Kamoliddin007 <chkb007147@gmail.com>
2020-03-02 11:44:58 +0500

    feat: incremental ids for education clones

--- js/validation.js
+++ js/validation.js
@@ -1,17 +1,17 @@
 var date = new Date();
 var currentyear = date.getFullYear();
 
-$('#from').yearselect({
+$("#from").yearselect({
   start: 2000,
-  order: 'desc',
+  order: "desc",
   end: currentyear
 });
-$('#to').yearselect({
+$("#to").yearselect({
   start: 2000,
-  order: 'desc',
+  order: "desc",
   end: currentyear
 });
-setTimeout(function () {
+setTimeout(function() {
   var x, i, j, selElmnt, a, b, c;
   /* Look for any elements with the class "custom-select": */
   x = document.getElementsByClassName("custom-select");
@@ -30,7 +30,7 @@ setTimeout(function () {
       create a new DIV that will act as an option item: */
       c = document.createElement("DIV");
       c.innerHTML = selElmnt.options[j].innerHTML;
-      c.addEventListener("click", function (e) {
+      c.addEventListener("click", function(e) {
         /* When an item is clicked, update the original select box,
         and the selected item: */
         var y, i, k, s, h;
@@ -53,7 +53,7 @@ setTimeout(function () {
       b.appendChild(c);
     }
     x[i].appendChild(b);
-    a.addEventListener("click", function (e) {
+    a.addEventListener("click", function(e) {
       /* When the select box is clicked, close any other select boxes,
       and open/close the current select box: */
       e.stopPropagation();
@@ -66,12 +66,15 @@ setTimeout(function () {
   function closeAllSelect(elmnt) {
     /* A function that will close all select boxes in the document,
     except the current select box: */
-    var x, y, i, arrNo = [];
+    var x,
+      y,
+      i,
+      arrNo = [];
     x = document.getElementsByClassName("select-items");
     y = document.getElementsByClassName("select-selected");
     for (i = 0; i < y.length; i++) {
       if (elmnt == y[i]) {
-        arrNo.push(i)
+        arrNo.push(i);
       } else {
         y[i].classList.remove("select-arrow-active");
       }
@@ -89,112 +92,129 @@ setTimeout(function () {
 /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
 
-
-var element = document.getElementById('number');
+var element = document.getElementById("number");
 var maskOptions = {
-  mask: '+{998}(90) 000-00-00'
+  mask: "+{998}(90) 000-00-00"
 };
 var mask = IMask(element, maskOptions);
 
-
-
-$('#info-form').submit(function () {
-
+$("#info-form").submit(function() {
   event.preventDefault();
-  var required_fields = document.getElementsByClassName('required');
+  var required_fields = document.getElementsByClassName("required");
   var error = false;
 
   for (let i = 0; i < required_fields.length; i++) {
     if (required_fields[i].value.length == 0) {
       error = true;
-      required_fields[i].parentElement.classList.add('warning');
+      required_fields[i].parentElement.classList.add("warning");
     }
   }
 
   // if (!error) {
-  let formData = new FormData(document.getElementById('info-form'));
-  
-  let educations = [{
-    entry_date: 'YYYY-MM-DD',
-    end_date: 'YYYY-MM-DD',
-    institution: 'string',
-    speciality: 'string',
-    course: 'string'  
-  }]
-  
-  let language_skills = [{
-    language: 'string',
-    skill: 'string'
-  }]
+  let formData = new FormData(document.getElementById("info-form"));
+
+  let educations = [
+    {
+      entry_date: "YYYY-MM-DD",
+      end_date: "YYYY-MM-DD",
+      institution: "string",
+      speciality: "string",
+      course: "string"
+    }
+  ];
+
+  let language_skills = [
+    {
+      language: "string",
+      skill: "string"
+    }
+  ];
 
   for (let [key, value] of formData.entries()) {
-    console.log(key, ':', value);
-
+    console.log(key, ":", value);
   }
 
-  formData.append('educations', educations)
-  formData.append('language_skills', language_skills)
+  formData.append("educations", educations);
+  formData.append(
+    "language_skills",
+    language_skills
+  )(
+    // }
 
-  // }
-
-  (function () {
-    setTimeout(function () {
-      $('.warning').removeClass('warning');
-    }, 5000)
-  })();
+    function() {
+      setTimeout(function() {
+        $(".warning").removeClass("warning");
+      }, 5000);
+    }
+  )();
 });
 
-
-var $fileInput = $('#file');
-var $droparea = $('#dragZone');
+var $fileInput = $("#file");
+var $droparea = $("#dragZone");
 
 // highlight drag area
-$fileInput.on('dragenter focus click', function () {
-  $droparea.addClass('is-active');
+$fileInput.on("dragenter focus click", function() {
+  $droparea.addClass("is-active");
 });
 
 // back to normal state
-$fileInput.on('dragleave blur drop', function () {
-  $droparea.removeClass('is-active');
+$fileInput.on("dragleave blur drop", function() {
+  $droparea.removeClass("is-active");
 });
 
 // change inner text
-$fileInput.on('change', function () {
+$fileInput.on("change", function() {
   var filesCount = $(this)[0].files.length;
-  var $valueHeader = $('#value-header');
-  var $textContainer = $('#file-value');
+  var $valueHeader = $("#value-header");
+  var $textContainer = $("#file-value");
 
   if (filesCount === 1) {
     // if single file is selected, show file name
-    var fileName = $(this).val().split('\\').pop();
-    $valueHeader.text('Выбранный вами файл:')
+    var fileName = $(this)
+      .val()
+      .split("\\")
+      .pop();
+    $valueHeader.text("Выбранный вами файл:");
     $textContainer.text(fileName);
   } else {
     // otherwise show number of files
-    $textContainer.text(filesCount + ' files selected');
+    $textContainer.text(filesCount + " files selected");
   }
 });
 
-var attrs = ['for', 'id', 'name'];
+var attrs = ["for", "id", "name"];
 
 function resetAttributeNames(section) {
-  var tags = section.find('input, label'),
+  var tags = section.find("input, label"),
     idx = section.index();
-  tags.each(function () {
+  tags.each(function() {
     var $this = $(this);
-    $.each(attrs, function (i, attr) {
+    $.each(attrs, function(i, attr) {
       var attr_val = $this.attr(attr);
       if (attr_val) {
-        $this.attr(attr, attr_val.replace(/_\d+$/, '_' + (idx + 1)))
+        $this.attr(attr, attr_val.replace(/_\d+$/, "_" + (idx + 1)));
       }
-    })
-  })
+    });
+  });
 }
 
-$('.add-education').click(function (e) {
+var eduAmount = 1;
+
+$(".add-education").click(function(e) {
   e.preventDefault();
-  var lastRepeatingGroup = $('.edu-inputs').last();
-  var cloned = lastRepeatingGroup.clone(true)
+
+  var lastRepeatingGroup = $(".edu-inputs").last();
+  var cloned = lastRepeatingGroup.clone(true);
+
+  eduAmount++;
+
+  cloned[0].querySelectorAll("input, select").forEach(input => {
+    let id = input.getAttribute("id");
+    let name = input.getAttribute("name");
+    id ? input.setAttribute("id", id + eduAmount) : "";
+    name ? input.setAttribute("name", name + eduAmount) : "";
+  });
+
   cloned.insertAfter(lastRepeatingGroup);
-  resetAttributeNames(cloned)
+  resetAttributeNames(cloned);
 });
